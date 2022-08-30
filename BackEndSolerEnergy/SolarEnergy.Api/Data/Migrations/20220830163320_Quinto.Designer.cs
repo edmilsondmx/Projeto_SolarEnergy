45061206647a51ed68d42b6b1be7eabd3541cae5
Edmilson Gomes <edmilsondmx@gmail.com>
2022-08-30 14:27:54 -0300

    feature; implementa migration com alteração nas models e banco de dados

--- BackEndSolerEnergy/SolarEnergy.Api/Data/Migrations/20220830163320_Quinto.Designer.cs
+++ BackEndSolerEnergy/SolarEnergy.Api/Data/Migrations/20220830163320_Quinto.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SolarEnergy.Api.Data;
 
@@ -10,9 +11,10 @@ using SolarEnergy.Api.Data;
 namespace SolarEnergy.Api.Data.Migrations
 {
     [DbContext(typeof(SolarDbContext))]
-    partial class SolarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220830163320_Quinto")]
+    partial class Quinto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
