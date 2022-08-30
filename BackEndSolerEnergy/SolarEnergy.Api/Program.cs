a1179dba9e53b6b6bbd0289853d606651bc9ed6f
Edmilson Gomes <edmilsondmx@gmail.com>
2022-08-30 12:24:44 -0300

    feature: alteração da descrição no swagger

--- BackEndSolerEnergy/SolarEnergy.Api/Program.cs
+++ BackEndSolerEnergy/SolarEnergy.Api/Program.cs
@@ -1,4 +1,5 @@
 using SolarEnergy.Api.Data;
+using Microsoft.OpenApi.Models;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
@@ -9,7 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( options => 
+{
+    options.SwaggerDoc("v1", 
+        new OpenApiInfo
+        {
+            Title = "SolarEnergy.Api",
+            Version = "v1",
+            Description = "Api desenvolvida para a Aplicação Solar Energy!",
+            Contact = new OpenApiContact
+            {
+                Name = "Edmilson Gomes",
+                Url = new Uri("https://github.com/edmilsondmx"),
+                Email = "edmilsondmx@gmail.com"
+            }
+        });
+});
 builder.Services.AddDbContext<SolarDbContext>();
 
 builder.Services.AddCors(options => 
