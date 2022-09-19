3ed7e1ca79e087bfcd79e0a65dcf46d683dcf662
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-19 16:36:06 -0300

    cria classe estática para segredo

---
+++ BackEndSolerEnergy/SolarEnergy.Domain/Security/Settings.cs
@@ -0,0 +1,6 @@
+namespace SolarEnergy.Domain.Security;
+
+public static class Settings
+{
+    public static readonly string Secret = "cf1eff33a2b53d9de85199a5430b13f5e8d924e8636aef5d95268bef4ec4f357";
+}
