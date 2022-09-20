1eec9fbe8b7a16396d3ba6338318a62a8f0fa249
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-20 13:35:42 -0300

    implementa refresh token

--- BackEndSolerEnergy/SolarEnergy.Api/Controllers/AutenticacaoController.cs
+++ BackEndSolerEnergy/SolarEnergy.Api/Controllers/AutenticacaoController.cs
@@ -2,31 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SolarEnergy.Domain.DTOs;
 using SolarEnergy.Domain.Interfaces.Services;
+using SolarEnergy.Domain.Services;
 
-namespace SolarEnergy.Api.Controllers
+namespace SolarEnergy.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AutenticacaoController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AutenticacaoController : ControllerBase
+    private readonly IUserService _userService;
+
+    public AutenticacaoController(IUserService userService)
     {
-        private readonly IUserService _userService;
+        _userService = userService;
+    }
 
-        public AutenticacaoController(IUserService userService)
-        {
-            _userService = userService;
-        }
+    [HttpPost]
+    [Route("login")]
+    public IActionResult Login(
+        [FromBody] LoginDto login
+    )
+    {
+        _userService.GetUser(login);
+        return Ok();
+    }
 
-        [HttpPost]
-        [Route("login")]
-        public IActionResult Login(
-            [FromBody] LoginDto login
-        )
-        {
-            _userService.ObterUsuario(login);
-            return Ok();
-        }
+    [HttpPost]
+    [Route("refresh-token")]
+    [AllowAnonymous]
+    public IActionResult Refresh(
+        [FromQuery] string token,
+        [FromQuery] string refreshToken
+    )
+    {
+        _userService.RefreshToken(token, refreshToken);
+        return Ok();
     }
 }
--- BackEndSolerEnergy/SolarEnergy.Domain/Services/TokenService.cs
+++ BackEndSolerEnergy/SolarEnergy.Domain/Services/TokenService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using SolarEnergy.Domain.Security;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Extensions;
+using System.Security.Cryptography;
 
 namespace SolarEnergy.Domain.Services;
 
@@ -34,5 +35,48 @@ public class TokenService
 
         var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
+    }
+
+    //Refresh token
+    public static ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Settings.Secret)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var secutiryToken);
+
+        if (secutiryToken is not JwtSecurityToken jwtSecurityToken)
+            throw new SecurityTokenException("Invalid token");
+
+        return principal;
+    }
+
+
+    public static string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    private static List<Tuple<string, string>> _refreshsTokens = new List<Tuple<string, string>>();
+
+    public static void SaveRefreshToken(string username, string refreshToken)
+        => _refreshsTokens.Add(new Tuple<string, string>(username, refreshToken));
+  
+    public static string GetRefreshToken(string username)
+        => _refreshsTokens.FirstOrDefault(x => x.Item1 == username).Item2;
+
+    public static void DeleteRefreshToken(string username, string refreshToken)
+    {
+        var item = _refreshsTokens.FirstOrDefault(x => x.Item1 == username && x.Item2 == refreshToken);
+        _refreshsTokens.Remove(item);
     }
 }
