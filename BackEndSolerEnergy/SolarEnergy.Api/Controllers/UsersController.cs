934254244425666008d9f625e371ee453c33e448
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-08 16:01:46 -0300

    feature: cria controller de uuarios

--- BackEndSolerEnergy/SolarEnergy.Api/Controllers/UsersController.cs
+++ BackEndSolerEnergy/SolarEnergy.Api/Controllers/UsersController.cs
@@ -1,15 +1,65 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
-namespace SolarEnergy.Api.Controllers
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SolarEnergy.Domain.DTOs;
+using SolarEnergy.Domain.Enuns;
+using SolarEnergy.Domain.Interfaces.Services;
+using SolarEnergy.Domain.Services;
+
+namespace SolarEnergy.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    private readonly IUserService _userService;
+    public UsersController(IUserService userService)
     {
-        
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public IActionResult ListUsers()
+    {
+        return Ok(ConverterRole.ToDto(_userService.Get()));
+    }
+
+    [HttpGet("{idUser}")]
+    public IActionResult ListById(
+        [FromRoute] int idUser
+    )
+    {
+        return Ok(ConverterRole.ToDto(_userService.GetById(idUser)));
+    }
+
+    [HttpPost]
+    public IActionResult CreateUser(
+        [FromBody] UserDto user
+    )
+    {
+        _userService.Post(user);
+        return Created("api/users", user);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPut("{idUser}")]
+    public IActionResult UpdateUser(
+        [FromRoute] int idUser,
+        [FromBody] UserDto user
+    )
+    {
+        user.Id = idUser;
+        _userService.Put(user);
+        return Ok();
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("{idUser}")]
+    public IActionResult DeleteUser(
+        [FromRoute] int idUser
+    )
+    {
+        _userService.Delete(idUser);
+        return NoContent();
     }
 }
