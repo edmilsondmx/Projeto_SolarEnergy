ddc8910e40379be5afb713ad9cd7acf5556bedbb
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-08 16:42:36 -0300

    fix: altera função de botao logoff para apagar token

--- FrontEndSolarEnergy/src/app/components/sidebar/sidebar.component.ts
+++ FrontEndSolarEnergy/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'pro-sidebar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  logoff(){
+    localStorage.clear();
+    this.router.navigate(['']);
   }
 
 }
