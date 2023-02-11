2b84c7e8b8faece9b45b66156c9d06ac9ad27418
Edmilson Gomes <edmilsondmx@gmail.com>
2023-02-10 21:42:07 -0300

    fix: cria mapeamento de logindto

--- BackEndSolerEnergy/SolarEnergy.Domain/DTOs/LoginDto.cs
+++ BackEndSolerEnergy/SolarEnergy.Domain/DTOs/LoginDto.cs
@@ -1,7 +1,24 @@
+using SolarEnergy.Domain.Models;
+
 namespace SolarEnergy.Domain.DTOs;
 
 public class LoginDto
 {
     public string Email { get; set; }
     public string Password { get; set; }
+
+    public LoginDto(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+    public LoginDto(Login login)
+    {
+        Email = login.Email;
+        Password = login.Password;
+    }
+    public LoginDto()
+    {
+
+    }
 }
